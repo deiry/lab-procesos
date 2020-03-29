1bcdc0bdd754df662f9d975a1314ba8c930c228d
deiry <sofia.navas96@gmail.com>
2020-03-28 19:35:43 -0500

    punto 4 done

---
+++ P4/punto4.c
@@ -0,0 +1,88 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<unistd.h>
+
+
+static const char *path = "/bin/ls";
+
+
+int execl_test(void);
+int execle_test(void);
+
+int execvp_test(void);
+int execlp_test(void);
+int execv_test(void);
+int execvp_test(void);
+int execvpe_test(void);
+
+
+int main(){
+
+    int child = fork();
+    if(child == 0){
+        printf("%s","Call execl");
+        //int r = execl_test();
+        int cle = execle_test();
+ 
+        int clp = execlp_test();
+        int cv = execv_test();
+        int cvp = execvp_test();
+        //int cvpe = execvpe_test();
+   
+        
+    }else {
+        wait(0);
+    }
+    return 0;
+
+}
+int execl_test(void){
+    char *arg1 = "-lh";   
+    int r= execl(path, path, arg1, NULL);
+
+  return r;
+}
+
+int execle_test(void){
+
+  char *arg1 = "-lh";
+
+
+  int r = execl(path, path, arg1, NULL);
+
+  return r;
+}
+
+
+
+int execlp_test(void){
+  char *arg1 = "-lh";
+  int r = execlp(path, path, arg1, NULL);
+  return r;
+}
+
+int execv_test(void){
+
+  char *args[] = {"-lh", NULL};
+  int r = execv(path, args);
+  return r;
+}
+
+
+int execvp_test(void){
+    char *args[] = { "-lh", NULL};
+    int r = execvp(path, args);
+  return r;
+}
+
+int execvpe_test(void){
+    char *args[] = { "-lh", NULL};
+    char *env[] = { "COLUMNS=5", NULL };
+    int r = execvpe(path, args, env);
+  return r;
+}
+
+//Obtenido de https://linuxhint.com/exec_linux_system_call_c/
+
+
+
